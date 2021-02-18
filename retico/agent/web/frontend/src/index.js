d9027b5756b2cd07364ae51b087a8d9224b2c8e9
Erik <eeckee@gmail.com>
2021-02-18 13:44:52 +0100

    good enough dialog visualization

--- retico/agent/web/frontend/src/index.js
+++ retico/agent/web/frontend/src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom";
 
 import WaveSurfer from "wavesurfer.js";
 import RegionPlugin from "wavesurfer.js/dist/plugin/wavesurfer.regions.min.js";
+import TimelinePlugin from "wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js";
 
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
+import Table from 'react-bootstrap/Table';
 // import { Collapse, Button} from 'react-bootstrap';
 // import {Button, Collapse} from 'react-bootstrap'
 
@@ -17,45 +19,136 @@ require("./index.css");
 // import { unregister } from "registerServiceWorker";
 
 const audioURL = "/api/audio";
-const fileURL = "/api/file";
+const dialogURL = "/api/file/dialog";
+const hparamsURL = "/api/file/hparams";
 
 
 function CollapseDiv(props) {
   const [open, setOpen] = useState(false);
 
-  let className = "turn_" + props.name;
+  // color 
+  let className = "turn-" + props.turn.name;
+  const entryStyle = {fontsize: 'small', overflowX: 'auto'}
 
-  let entries = [
-    <div className='row turn-info-text'> Start: {props.startTime}</div>,
-    <div className='row turn-info-text'> End: {props.endTime}</div>,
-  ];
-
-  if (props.trp) {
-    entries.push(
-      <div className='row turn-info-text'> TRP: {props.trp} </div>
-    );
-  }
-
-  if (props.completion) {
-    entries.push(
-      <div className='row turn-info-text'> Completion: {props.completion}</div>
-    );
-    // entries.push(
-    //   <div className='row turn-info-text'>
-    //     Planned: {props.planned_utterance}
-    //   </div>
-    // );
+  let listItems = [];
+  if (props.turn !== undefined) {
+    let i=0
+    let key;
+    let val;
+    for (let p in props.turn) {
+      if (props.turn[p] !== undefined) {
+        if (p === 'all_trps'){
+          const trp = props.turn.all_trps.map((trp, i) => {
+            if (i > 0) {
+              return ", " + trp.trp.toString()
+            } else {
+              return trp.trp.toString()
+            }
+          })
+          listItems.push(
+            <li key={key} style={entryStyle}>
+              {p}: {trp}
+            </li>
+          )
+        } else {
+          listItems.push(
+            <li key={key} style={entryStyle}>
+              {p}: {props.turn[p].toString()}
+            </li>
+          )
+        }
+        i ++;
+      }
+    }
   }
 
   return (
     <div>
-      <Button onClick={() => setOpen(!open)} className={className} >
-        {props.utterance}
+      <Button onClick={() => {
+        if (!open) {
+          props.onClick(props.turn.start_time)
+        }
+        setOpen(!open)
+        // console.log(props.turn.start_time)
+        }} className={className} >
+        {props.turn.utterance}
       </Button>
       <Collapse in={open}>
-        <div className='col'> {entries} </div>
+        <ol style={{listStyleType: 'none'}}>{listItems}</ol>
       </Collapse>
     </div>
+  );
+}
+
+function Header(props) {
+  // list hparam entries in a table
+  let table = "";
+  if (props.state.hparams !== undefined) {
+    let listItems = [];
+    const hparams = props.state.hparams;
+    let key;
+    let i=0;
+    for (let p in hparams) {
+      key = 'hparams'+i.toString();
+      listItems.push(
+        <tr key={key}> 
+          <td>{p}</td> 
+          <td>{hparams[p].toString()}</td> 
+        </tr>
+      )
+      i ++;
+    }
+    if (listItems.length > 0){
+      table = (
+        <Table responsive striped hover borderless variant="dark" responsive="sm" size="sm" style={{borderRadius: "5px", fontSize: "small"}}>
+          <tbody>
+          {listItems} 
+          </tbody>
+        </Table>
+      )
+    }
+  }
+
+  return (
+    <Container style={{margin: '10px'}}>
+      <h1>Dialog Visualization</h1>
+      <div className='row'> 
+        <div className='col'> 
+          <div className="controls"> 
+            <ol style={{listStyleType: 'none'}}>
+              <li>
+                <input type="checkbox" value="showVad" checked={props.state.showVad} onChange={e => props.onControlChange("vad")}/>
+                VAD
+              </li>
+              <li>
+                <input type="checkbox" value="showAsr" checked={props.state.showAsr} onChange={e => props.onControlChange("asr")}/>
+                ASR
+              </li>
+              <li>
+                <input type="checkbox" value="showAsr" checked={props.state.showAgentTurn} onChange={e => props.toggleAgentTurn()}/>
+                AgentTurnOn
+              </li>
+              <li>
+                <input type="checkbox" value="showAsr" checked={props.state.showTRP} onChange={e => props.toggleTRP()}/>
+                All TRP
+              </li>
+              <li>
+                <input type="checkbox" value="showAsr" checked={props.state.showAgentInterrupt} onChange={e => props.toggleAgentInterrupt()}/>
+                AgentInterrupted
+              </li>
+              <li>
+                <input type="checkbox" value="showAsr" checked={props.state.showDialogStates} onChange={e => props.toggleDialogStates()}/>
+                DialogStates
+              </li>
+            </ol>
+            Zoom <input type="range" min="50" max="300" style={{width: "300px"}} onChange={e => props.onZoom(e)}/>
+          </div>
+        </div>
+        <div className='col' style={{height: "270px", overflowY: 'scroll'}}> 
+          {table}
+        </div>
+      </div>
+    </Container >
   );
 }
 
@@ -66,8 +159,19 @@ class Dialog extends Component {
       dialog: null,
       showVad: false,
       showAsr: false,
-      zoom: 0,
+      showAgentTurn: false,
+      showTRP: false,
+      showAgentInterrupt: false,
+      showDialogStates: false,
+      zoom: 50,
     };
+    this.onZoom=this.onZoom.bind(this)
+    this.onControlChange=this.onControlChange.bind(this)
+    this.toggleAgentTurn=this.toggleAgentTurn.bind(this)
+    this.toggleTRP=this.toggleTRP.bind(this)
+    this.toggleAgentInterrupt=this.toggleAgentInterrupt.bind(this)
+    this.toggleDialogStates=this.toggleDialogStates.bind(this)
+    this.onTimeClick=this.onTimeClick.bind(this)
   }
 
   // Waveform + AudioPlayer
@@ -89,13 +193,22 @@ class Dialog extends Component {
       forceDecode: true,
       normalize: true,
       responsive: true,
-      plugins: [RegionPlugin.create()],
+      plugins: [
+        RegionPlugin.create(),
+        TimelinePlugin.create({
+          container: "#wave-timeline",
+    })
+      ],
     });
     this.waveform.load(audioURL);
+    this.waveform.zoom(this.state.zoom);
 
-    fetch(fileURL).then(response => response.json()).then((data) => {
-      console.log(data);
+    fetch(dialogURL).then(response => response.json()).then((data) => {
       this.setState({dialog: data})
+    })
+    fetch(hparamsURL).then(response => response.json()).then((data) => {
+      this.setState({hparams: data})
+      // console.log(data);
     })
   }
 
@@ -120,11 +233,55 @@ class Dialog extends Component {
     }
   }
 
+  drawDialogStates(states) {
+    let curState, start, end, color;
+    for (var i = 0, len = states.length-1; i < len; i++) {
+      curState = states[i].state
+      start = states[i].time
+      end = states[i+1].time
+      if (curState === "only_agent") {
+        color = "#11aa1110"
+      } else if (curState === "only_user") {
+        color = "#1111aa10"
+      } else if (curState === "silence") {
+        color = "#ffffff10"
+      } else if (curState === "both_active") {
+        color = "#aa111110"
+      }
+
+      this.waveform.addRegion({
+        start: start,
+        end: end,
+        color: color,
+        drag: false,
+        resize: false,
+        id: 'dialogState' + i,
+      });
+    }
+  }
+
+  drawLine (start, color, id, width) {
+    this.waveform.addRegion({
+      start: start,
+      end: start+width,
+      color: color,
+      drag: false,
+      resize: false,
+      id: id
+    })
+  }
+
+  drawLines(arr, color, id, width=0.05) {
+    let idd;
+    for (var i = 0, len = arr.length; i < len; i++) {
+      idd = id + 'Region' + i;
+      this.drawLine(arr[i], color, idd, width);
+    }
+  }
+
   onClear() {
     let regs = this.waveform.regions.list;
     for (const region in regs) {
-      console.log(region);
-      console.log(regs[region]);
       regs[region].remove();
     }
   }
@@ -132,18 +289,85 @@ class Dialog extends Component {
   removeRegion(id) {
     let regs = this.waveform.regions.list;
     for (const region in regs) {
-      if (regs[region].id.includes(id+'Region')) {
+      if (regs[region].id.includes(id)) {
         regs[region].remove();
       };
     }
   };
 
+  toggleAgentTurn() {
+    if (this.state.dialog === null) {
+      return;
+    }
+    if (this.state.showAgentTurn) {
+      this.removeRegion('agentTurnOn')
+      this.setState({showAgentTurn: false})
+    } else {
+      this.drawLines(this.state.dialog.agent_turn_on, "#221199", 'agentTurnOn')
+      this.setState({showAgentTurn: true})
+    };
+  }
+
+  toggleAgentInterrupt() {
+    if (this.state.dialog === null) {
+      return;
+    }
+    if (this.state.showAgentInterrupt) {
+      this.removeRegion('agentInterrupt')
+      this.setState({showAgentInterrupt: false})
+    } else {
+      this.drawLines(this.state.dialog.agent_interrupted, "#99112210", 'agentInterrupt', 0.3)
+      this.setState({showAgentInterrupt: true})
+    };
+  }
+
+  toggleTRP() {
+    if (this.state.dialog === null) {
+      return;
+    }
+
+    if (this.state.showTRP) {
+      this.removeRegion('trp')
+      this.setState({showTRP: false})
+    } else {
+      let color;
+      let n = 0;
+      let trps = [];
+      this.setState({showTRP: true})
+      this.state.dialog.turns.forEach(turn => {
+        if (turn.all_trps !== undefined) {
+          turn.all_trps.forEach(trp => {
+            if (trp.trp < this.state.hparams.trp) {
+              color = "#aa0000"
+            } else {
+              color = "#00aa00"
+            }
+            this.drawLine(trp.time, color, 'trp' + n.toString(), 0.05);
+            n ++;
+          })
+        }
+      })
+    };
+  }
+
+  toggleDialogStates() {
+    if (this.state.dialog === null) {
+      return;
+    }
+    if (this.state.showDialogStates) {
+      this.removeRegion('dialogState')
+      this.setState({showDialogStates: false})
+    } else {
+      this.drawDialogStates(this.state.dialog.dialog_states)
+      this.setState({showDialogStates: true})
+    };
+  }
+
   onControlChange(name) {
-    console.log(name)
     if (this.state.dialog !== null) {
       if (name === "vad") {
         if (this.state.showVad) {
-          this.removeRegion(name)
+          this.removeRegion(name+'Region')
           this.setState({showVad: false})
         } else {
           this.drawRegion(this.state.dialog.vad_ipu_on, this.state.dialog.vad_ipu_off, "#81c78430", 'vad')
@@ -151,7 +375,7 @@ class Dialog extends Component {
         };
       } else if (name === "asr"){
         if (this.state.showAsr) {
-          this.removeRegion(name)
+          this.removeRegion(name+'Region')
           this.setState({showAsr: false})
         } else {
           this.drawRegion(this.state.dialog.asr_on, this.state.dialog.asr_off, "#28309010", 'asr')
@@ -166,19 +390,15 @@ class Dialog extends Component {
     if (dialog !== null) {
       return (
         dialog.turns.map((turn, i) => {
-          console.log('turn_' + i.toString())
-
+          let side='left';
+          if (turn.name === 'agent') {
+            side='right'
+          }
           return (
-            <li key={'turn_' + i.toString()}>
+            <li key={'turn_' + i.toString()} >
               <CollapseDiv 
-                utterance={turn.utterance}
-                name={turn.name}
-                endTime={ turn.end_time }
-                startTime={ turn.start_time }
-                trp={turn.trp_at_eot}
-                completion={turn.completion}
-                planned_utterance={turn.planned_utterance}
-                onTimeClick={this.onTimeClick}
+                turn={turn}
+                onClick={this.onTimeClick}
               />
             </li>
           )
@@ -191,23 +411,21 @@ class Dialog extends Component {
     const turns = this.getTurns();
     return (
       <Container>
+        <Header 
+          onZoom={this.onZoom}
+          onControlChange={this.onControlChange}
+          toggleAgentTurn={this.toggleAgentTurn}
+          toggleTRP={this.toggleTRP}
+          toggleAgentInterrupt={this.toggleAgentInterrupt}
+          toggleDialogStates={this.toggleDialogStates}
+          state={this.state}
+        />
+        <div id="wave-timeline"></div>
         <div id="audioplayer"></div>
-        <div className="controls"> 
-          <input type="range" min="50" max="500" style={{width: "400px"}} onChange={e => this.onZoom(e)}/>
-          <br/>
-          <input type="checkbox" value="showVad" checked={this.state.showVad} onChange={e => this.onControlChange("vad")}/>
-          VAD
-          <br/>
-          <input type="checkbox" value="showAsr" checked={this.state.showAsr} onChange={e => this.onControlChange("asr")}/>
-          ASR
-        </div>
-        <div className="dialog">
-          <ol>{turns}</ol>
-        </div>
-        <div>
-          <Button onClick={() => {this.onTimeClick(5)}}>
-            Start: 5
-          </Button>
+        <div className='row'>
+          <div className="dialog">
+            <ol style={{listStyleType: 'none'}}>{turns}</ol>
+          </div>
         </div>
       </Container>
     );
